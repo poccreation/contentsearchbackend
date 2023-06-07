489841b71c18975ebcfe2a2ae43a81e1b938fc80
ShubhamGupta <shubhamgupta@ShubhamGuptas-MacBook-Air.local>
2023-06-07 17:28:37 +0530

    cross origin

--- boogle/src/main/java/ca/sunlife/poc/boogle/rest/BoogleRestController.java
+++ boogle/src/main/java/ca/sunlife/poc/boogle/rest/BoogleRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +19,7 @@ import ca.sunlife.poc.boogle.response.QueryResponse;
 import ca.sunlife.poc.boogle.sharepoint.service.ISharepointSearchService;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = "${boogle.base.url}")
 public class BoogleRestController {
 
