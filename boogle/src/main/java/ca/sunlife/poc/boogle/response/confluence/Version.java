9ea3d365cecb1de64935b0250e07256f7e59351a
ShubhamGupta <shubhamgupta@ShubhamGuptas-MacBook-Air.local>
2023-06-07 17:34:00 +0530

    Verison.java

---
+++ boogle/src/main/java/ca/sunlife/poc/boogle/response/confluence/Version.java
@@ -0,0 +1,15 @@
+package ca.sunlife.poc.boogle.response.confluence;
+
+public class Version {
+
+	private String when;
+
+	public String getWhen() {
+		return when;
+	}
+
+	public void setWhen(String when) {
+		this.when = when;
+	}
+
+}
